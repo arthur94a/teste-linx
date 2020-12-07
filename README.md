8be70f7b41702564438ac606b8b7948bc089fb58
arthur94a <arthuraraujo94@hotmail.com>
2020-12-07 19:16:55 -0300

    README's update

--- README.md
+++ README.md
@@ -12,37 +12,39 @@
 
 
 
-## 🔥 | SOBRE
+## 🔥 | ABOUT
 
-Projeto para o desafio elaborado pela **Linx**, sendo criado um site responsivo baseado em um layout pré determinado. O objetivo é construir uma **aplicação de eCommerce** que contenha uma grade de produtos e um formulario newsletter.
-
-__ 
-
-#### *Project for the challenge prepared by **Linx**, creating a responsive website based on a predetermined layout. The goal is to build an **eCommerce application** that contains a product grid and a newsletter form.*
+#### Project for the challenge prepared by **Linx**, creating a responsive website based on a predetermined layout. The goal is to build an **eCommerce application** that contains a product grid and a newsletter form.
 
 ##### [Layout link]( https://xd.adobe.com/spec/4025e242-a495-4594-71d2-5fd89d774b57-3614) | [EndPoint da API]( https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1)
 
 ---
 
-## 🐱‍👤| TÉCNOLOGIAS UTILIZADAS
-
-O projeto foi desenvolvido com as tecnologias seguintes:
+## 🐱‍👤| STACK TECHNOLOGIES
 
 
-#### *Developed with the following technologies*
+#### Developed with the following technologies
 
 - [HTML5](https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5)
 - [CSS3](https://developer.mozilla.org/en-US/docs/Web/CSS)
 - [JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
 
 ---
-## EXECUTANDO
-Abra o arquivo **index.html** e o execute! :D
+## RUNNING
 
-#### *Open **index.html** on the browser*
+
+site: https://ecommerce-linx.vercel.app/
+#### email: https://ecommerce-linx.vercel.app/emailmarketing.html
+
+__
+
+As there is a restriction on an email page in relation to additional files such as CSS and JavaScript, there was a need to create all the **html** logic in a single file to the maximum to avoid rendering errors.
+
+
+
 
 ---
-<p align="right">Criado por Arthur Araujo.</p>
+<p align="right">Created by Arthur Araujo.</p>
 
 
 
--- main.js
+++ main.js
@@ -6,76 +6,73 @@ function loadProducts(url) {
   .then(data => {
     data.products.forEach(prd => {
       
-      /*estrutura da div do produto*/
+     
       const product = `
         <div class="products-item">
-    <div>
-          <img class="products-img" src="http:${prd.image}" />
-    </div>
-    <div class="products-item-justResponsive">      
-          <p class="product-name">${prd.name}</p>
-          <p class="product-description">${prd.description}</p>
-          <p class="product-old-price">De: R$${prd.oldPrice.toFixed(2)}</p> 
-          <p class="product-price">Por: R$${prd.price.toFixed(2)}</p>
-          <p class="product-installments">ou ${prd.installments.count}x de R$${prd.installments.value.toFixed(2)}</p>
-          <button class="send-button">Comprar</button>
-    </div>      
+            <div>
+                  <img class="products-img" src="http:${prd.image}" />
+            </div>
+          
+            <div class="products-item-justResponsive">      
+                  <p class="product-name">${prd.name}</p>
+                  <p class="product-description">${prd.description}</p>
+                  <p class="product-old-price">De: R$${prd.oldPrice.toFixed(2)}</p> 
+                  <p class="product-price">Por: R$${prd.price.toFixed(2)}</p>
+                  <p class="product-installments">ou ${prd.installments.count}x de R$${prd.installments.value.toFixed(2)}</p>
+                  <button class="send-button">Comprar</button>
+            </div>      
         </div>
       `;
-    /* toFixe(2) -> Usado para acrescentar a fração ao valor 
-     
-    /*função carregar produto por ID*/  
+ 
       const productsElement = document.getElementById('products');
       productsElement.insertAdjacentHTML( 'beforeend', product );
     });
 
-    /*Função gerar produtos */
-    const loadMoreElement = document.getElementById('load-more-products');
-    loadMoreElement.setAttribute("onclick", `javascript: loadProducts('https://${data.nextPage}');`);
+      const loadMoreElement = document.getElementById('load-more-products');
+      loadMoreElement.setAttribute("onclick", `javascript: loadProducts('https://${data.nextPage}');`);
 
 
   });
-}
+  }
 
-//Validação do email--------------------------------------------------------------
-function validacaoEmail(field) {
+function emailValidation(field) {
   usuario = field.value.substring(0, field.value.indexOf("@"));
   dominio = field.value.substring(field.value.indexOf("@")+ 1, field.value.length);
   
-  if ((usuario.length >=1) &&
-      (dominio.length >=3) &&
-      (usuario.search("@")==-1) &&
-      (dominio.search("@")==-1) &&
-      (usuario.search(" ")==-1) &&
-      (dominio.search(" ")==-1) &&
-      (dominio.search(".")!=-1) &&
-      (dominio.indexOf(".") >=1)&&
-      (dominio.lastIndexOf(".") < dominio.length - 1)) {
-  document.getElementById("msgemail").innerHTML="E-mail válido";
-  }
-  else{
-  document.getElementById("msgemail").innerHTML="<font color='red'>E-mail inválido </font>";
-  }
+      if ((usuario.length >=1) &&
+          (dominio.length >=3) &&
+          (usuario.search("@")==-1) &&
+          (dominio.search("@")==-1) &&
+          (usuario.search(" ")==-1) &&
+          (dominio.search(" ")==-1) &&
+          (dominio.search(".")!=-1) &&
+          (dominio.indexOf(".") >=1)&&
+          (dominio.lastIndexOf(".") < dominio.length - 1)) {
+      document.getElementById("msgemail").innerHTML="E-mail válido";
+      }
+      else{
+      document.getElementById("msgemail").innerHTML="<font color='red'>E-mail inválido </font>";
+      }
   }
 
-  function validacaoEmail2(field) {
+function emailValidation2(field) {
     usuario = field.value.substring(0, field.value.indexOf("@"));
     dominio = field.value.substring(field.value.indexOf("@")+ 1, field.value.length);
     
-    if ((usuario.length >=1) &&
-        (dominio.length >=3) &&
-        (usuario.search("@")==-1) &&
-        (dominio.search("@")==-1) &&
-        (usuario.search(" ")==-1) &&
-        (dominio.search(" ")==-1) &&
-        (dominio.search(".")!=-1) &&
-        (dominio.indexOf(".") >=1)&&
-        (dominio.lastIndexOf(".") < dominio.length - 1)) {
-    document.getElementById("msgemail2").innerHTML="E-mail válido";
-    }
-    else{
-    document.getElementById("msgemail2").innerHTML="<font color='red'>E-mail: inválido </font>";
-    }
-    }
+      if ((usuario.length >=1) &&
+          (dominio.length >=3) &&
+          (usuario.search("@")==-1) &&
+          (dominio.search("@")==-1) &&
+          (usuario.search(" ")==-1) &&
+          (dominio.search(" ")==-1) &&
+          (dominio.search(".")!=-1) &&
+          (dominio.indexOf(".") >=1)&&
+          (dominio.lastIndexOf(".") < dominio.length - 1)) {
+      document.getElementById("msgemail2").innerHTML="E-mail válido";
+      }
+      else{
+      document.getElementById("msgemail2").innerHTML="<font color='red'>E-mail: inválido </font>";
+      }
+  }
 
   window.onload = loadProducts(apiUrl);
